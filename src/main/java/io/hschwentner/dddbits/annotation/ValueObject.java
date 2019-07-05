585b9f50b11c6bb061e83971f5ece6f9a60ad371
Henning Schwentner <hs@wps.de>
2019-07-05 19:01:09 +0200

    Added annotation for value types

---
+++ src/main/java/io/hschwentner/dddbits/annotation/ValueObject.java
@@ -0,0 +1,11 @@
+package io.hschwentner.dddbits.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Documented
+public @interface ValueObject {
+
+}
