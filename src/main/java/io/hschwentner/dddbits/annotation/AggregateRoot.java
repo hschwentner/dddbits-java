37f86c6013c054c45473ee37be6e09aca43da906
Henning Schwentner <hs@wps.de>
2019-07-05 18:56:57 +0200

    Initial drop of the annotations

---
+++ src/main/java/io/hschwentner/dddbits/annotation/AggregateRoot.java
@@ -0,0 +1,12 @@
+package io.hschwentner.dddbits.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Documented
+@DomainEntity
+public @interface AggregateRoot {
+
+}
---
+++ src/main/java/io/hschwentner/dddbits/annotation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains annotations to annotate elements as building blocks in DDD meaning.
+ * 
+ * @author Henning Schwentner, WPS
+ *
+ */
+package io.hschwentner.dddbits.annotation;
