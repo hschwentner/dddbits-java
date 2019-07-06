3a7b4330c12a8f81ddff0d74e9e9cff774c60b67
Henning Schwentner <hs@wps.de>
2019-07-06 12:30:43 +0200

    Added annotation for building block "Aggregate"

---
+++ src/main/java/io/hschwentner/dddbits/annotation/Aggregate.java
@@ -0,0 +1,12 @@
+package io.hschwentner.dddbits.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+//@Target({ElementType.PACKAGE, ElementType.MODULE})
+@Target({ElementType.PACKAGE})
+@Documented
+public @interface Aggregate {
+
+}
