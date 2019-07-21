975944c40b32721576ae0713ab80f58ae02d8a95
Henning Schwentner <hs@wps.de>
2019-07-21 22:48:42 +0200

    Add author tag

--- src/main/java/io/hschwentner/dddbits/annotation/ValueObject.java
+++ src/main/java/io/hschwentner/dddbits/annotation/ValueObject.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Target;
 /**
  * Represents a value of the domain.
  * A value has no identity and is immutable.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
  */
 @Target(ElementType.TYPE)
 @Documented
