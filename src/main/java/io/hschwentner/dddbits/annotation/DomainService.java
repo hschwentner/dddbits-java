5d1cd8f131a8a8e9a94b252d4d79b35a5c70397e
Henning Schwentner <hs@wps.de>
2019-07-22 17:54:52 +0200

    Add JavaDoc comment for DomainService

--- src/main/java/io/hschwentner/dddbits/annotation/DomainService.java
+++ src/main/java/io/hschwentner/dddbits/annotation/DomainService.java
@@ -4,6 +4,14 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a type as a domain service.
+ * Such a service contains only domain logic.
+ * It keeps no own state, although it typically changes the state of the {@link Aggregate}s it is working on.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 @Target(ElementType.TYPE)
 @Documented
 public @interface DomainService {
