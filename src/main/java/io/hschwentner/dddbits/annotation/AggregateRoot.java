9675300955f3392a4ab9d9a96d3a5ba17134217d
Henning Schwentner <hs@wps.de>
2019-07-27 00:15:39 +0200

    Add JavaDoc comment for AggregateRoot

--- src/main/java/io/hschwentner/dddbits/annotation/AggregateRoot.java
+++ src/main/java/io/hschwentner/dddbits/annotation/AggregateRoot.java
@@ -4,6 +4,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a class as the root entity of an aggregate.
+ * Inner entities of the aggregate may only be accessed through this root entity.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 @Target(ElementType.TYPE)
 @Documented
 @DomainEntity
