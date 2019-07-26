b0412d335008f5589faeda66858581be8d0c617f
Henning Schwentner <hs@wps.de>
2019-07-26 14:12:43 +0200

    Add JavaDoc comment for AggregateRoot

--- src/main/java/io/hschwentner/dddbits/annotation/Aggregate.java
+++ src/main/java/io/hschwentner/dddbits/annotation/Aggregate.java
@@ -4,6 +4,14 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a package as an aggregate of the domain.
+ * An aggregate draws a boundary around its entity or entities.
+ * Inner entities may only be accessed through the root entity.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 //@Target({ElementType.PACKAGE, ElementType.MODULE})
 @Target({ElementType.PACKAGE})
 @Documented
