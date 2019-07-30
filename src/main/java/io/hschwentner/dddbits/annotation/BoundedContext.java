cebe97527f9c01edb3917c1d1d1c93cacda33132
Henning Schwentner <hs@wps.de>
2019-07-30 17:33:05 +0200

    Add JavaDoc comment for BoundedContext

--- src/main/java/io/hschwentner/dddbits/annotation/BoundedContext.java
+++ src/main/java/io/hschwentner/dddbits/annotation/BoundedContext.java
@@ -4,6 +4,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a package as a bounded context.
+ * The domain model in the bounded context may only be accessed through its interface.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 //@Target({ElementType.MODULE, ElementType.PACKAGE})
 @Target({ElementType.PACKAGE})
 @Documented
