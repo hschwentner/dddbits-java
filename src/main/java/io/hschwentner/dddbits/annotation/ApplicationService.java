44968d498ec086d2739a29d1b795a96b4d334b5c
Henning Schwentner <hs@wps.de>
2019-07-30 17:16:31 +0200

    Add JavaDoc comment for ApplicationService

--- src/main/java/io/hschwentner/dddbits/annotation/ApplicationService.java
+++ src/main/java/io/hschwentner/dddbits/annotation/ApplicationService.java
@@ -4,6 +4,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a class as a service of the application layer.
+ * Not to be confused with {@link DomainService}.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 @Target(ElementType.TYPE)
 @Documented
 public @interface ApplicationService {
