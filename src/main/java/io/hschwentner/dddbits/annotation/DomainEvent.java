b30ffaba748df1db95a7ceb07491960d7475d289
Henning Schwentner <hs@wps.de>
2019-07-31 11:38:30 +0200

    Add JavaDoc for event and module

--- src/main/java/io/hschwentner/dddbits/annotation/DomainEvent.java
+++ src/main/java/io/hschwentner/dddbits/annotation/DomainEvent.java
@@ -4,6 +4,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a type as a domain event.
+ * An event indicates that something has happened that is relevant to domain experts.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 @Target(ElementType.TYPE)
 @Documented
 public @interface DomainEvent {
--- src/main/java/io/hschwentner/dddbits/annotation/DomainModule.java
+++ src/main/java/io/hschwentner/dddbits/annotation/DomainModule.java
@@ -4,6 +4,12 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a package as a module from the domain.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 //@Target({ElementType.PACKAGE, ElementType.MODULE})
 @Target({ElementType.PACKAGE})
 @Documented
