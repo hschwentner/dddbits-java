dc6fd5edcbfeeb3ec5202d20d6dd0c5a52e05af8
Henning Schwentner <hs@wps.de>
2019-07-31 12:24:01 +0200

    Add JavaDoc for factory

--- src/main/java/io/hschwentner/dddbits/annotation/Factory.java
+++ src/main/java/io/hschwentner/dddbits/annotation/Factory.java
@@ -4,6 +4,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a type as a factory.
+ * A factory is an element of the domain layer that creates domain objects.
+ *
+ * @author Henning Schwentner
+ * @since 1.0
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Documented
 public @interface Factory {
